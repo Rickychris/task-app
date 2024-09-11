832bcca4c3011fcd4336e9b2153eb367e60e1239
Ricky Chris <rickychris6@gmail.com>
2024-09-11 07:56:54 +0400

    auth and db relation

--- src/models/task.js
+++ src/models/task.js
@@ -14,7 +14,14 @@ const taskSchema = new mongoose.Schema({
     completed: {
         type: Boolean,
         default: false,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
+}, {
+    timestamps: true
 })
 
 const Task = mongoose.model('Task', taskSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,8 @@ const validator = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const Task = require('./task');
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -49,7 +51,23 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 })
+
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
@@ -82,6 +100,14 @@ userSchema.pre('save', async function (next) {
     }
     next()
 })
+
+// delete user tasks when user is removed
+userSchema.pre('deleteOne', async function (next) {
+    const user = this.getQuery()
+    await Task.deleteMany({ owner: user._id })
+    next()
+})
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User
